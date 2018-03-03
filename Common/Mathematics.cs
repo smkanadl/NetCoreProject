3e55dfab46995d714b36c16f82347dbdc92bcb4c
Kai Nadler <kai.nadler@googlemail.com>
2018-03-03 11:28:31 +0100

    refactor: Add xml doc to Common lib to silence some warnings.

--- Common/Mathematics.cs
+++ Common/Mathematics.cs
@@ -2,8 +2,17 @@
 
 namespace Leybold.Common
 {
+    /// <summary>
+    /// A class which implements some basic algebra.
+    /// </summary>
     public class Mathematics
     {
+        /// <summary>
+        /// Calculate the sum of two given numbers.
+        /// </summary>
+        /// <param name="l">First operand of sum operation.</param>
+        /// <param name="r">Second operand of sum operation.</param>
+        /// <returns>The sum of both operands.</returns>
         public static double Sum(double l, double r)
         {
             return l + r;
