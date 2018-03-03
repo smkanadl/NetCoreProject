ca3612bdfed921ca86688a4311938a13be43e82b
Kai Nadler <kai.nadler@googlemail.com>
2018-03-03 11:28:06 +0100

    refactor: Replace WpfApp with a .net framework console app to be portable.

---
+++ NetApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Leybold.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mathematics.Sum(3, 5));
+        }
+    }
+}
